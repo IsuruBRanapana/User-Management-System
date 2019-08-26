5e98652c4da4fc4d11124c137ccc4a2f21c81910
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-26 22:16:37 +0530

    Create connection.php
    connection page created

---
+++ inc/connection.php
@@ -0,0 +1,12 @@
+<?php  
+	$server='localhost';
+	$user='root';
+	$dbname='umsdb';
+	mysqli_connect($server,$user,'',$dbname);
+
+	if(mysqli_connect_errno()){
+		echo ("Can't connect to database. ".mysqli_connect_error());
+	}else{
+		echo "Connection Established";
+	}
+?>
